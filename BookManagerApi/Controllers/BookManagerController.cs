b787acf5fb122d4497f3cabed6739de4943df510
Shery Shajan <sheryjebu@gmail.com>
2022-07-04 22:26:23 +0100

    APIconnectingToDataBase

--- BookManagerApi/Controllers/BookManagerController.cs
+++ BookManagerApi/Controllers/BookManagerController.cs
@@ -47,7 +47,7 @@ namespace BookManagerApi.Controllers
             _bookManagementService.Create(book);
             return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
         }
-        [HttpPost]
+        [HttpDelete("{id}")]
         public ActionResult<Book> DeleteBook(long id, Book book)
         {
             _bookManagementService.Delete(id, book);
